babcc7ea046e4b33b1d4a32634fd8e9bc10d7fe0
Bohdan <usik_13@ukr.net>
2014-10-27 00:51:01 +0200

    some js logic, page numbers, modals, arrows up/down

--- javascripts/index.js
+++ javascripts/index.js
@@ -15,16 +15,23 @@ $( document ).ready(function() {
   }
 
   $('#fullpage').fullpage({
-    resize: true,
+    resize: false,
     css3: true,
     scrollingSpeed: 1,
     onLeave: function(index, nextIndex, direction){
-      if(index < 10) {
+      if(index > 2 && direction == 'up') {
+        $(".previous-photo-button").addClass('visible');
+        setTimeout(function(){
+          $(".previous-photo-button").removeClass('visible');
+        }, 1000)
+      }
+      else if(direction == 'down') {
         $(".next-photo-button").addClass('visible');
         setTimeout(function(){
           $(".next-photo-button").removeClass('visible');
         }, 1000)
       }
+
       if(index == 2 && direction == 'up'){
         makeVisibleNav();
       }
@@ -36,6 +43,9 @@ $( document ).ready(function() {
   $(".next-photo-button").on('click', function(){
     $.fn.fullpage.moveSectionDown();
   });
+  $(".previous-photo-button").on('click', function(){
+    $.fn.fullpage.moveSectionUp();
+  });
   $(".menu-triger").on('click', function () {
     if ($('.navbar-fixed-top').hasClass('visible-nav')) {
       makeHiddenNav();
@@ -46,9 +56,22 @@ $( document ).ready(function() {
   setTimeout(function(){
     $(".next-photo-button").removeClass('visible');
   }, 2000)
-  $window = $(window);
-  $('.section').on('click', function(e){
+  $("img").load(function() {
+      var screewidth = $(window).width();
+      var width = $(this).width();
+      $.each($(this), function(i,val){
+        if ($(window).width()/$(window).height() < $(this).width()/$(this).height()) {
+          $(this).removeClass('img-full-height');
+          $(this).addClass('img-full-width');
+        }
+      });
 
+    });
+  $('.portfolio-modal').on('show.bs.modal',function(){
+    $.fn.fullpage.setAllowScrolling(false);
+  })
+  $('.portfolio-modal').on('hide.bs.modal',function(){
+    $.fn.fullpage.setAllowScrolling(true);
   })
 
 });
